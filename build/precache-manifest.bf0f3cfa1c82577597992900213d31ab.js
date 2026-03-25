ecc7d0187f3fd95a2c997432d4d59d62a9e743dd
vanhauluonsuy <luuvanhau1611207@gmail.com>
2026-03-26 01:41:40 +0700

    first commit

---
+++ build/precache-manifest.bf0f3cfa1c82577597992900213d31ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c16fbd951beb27ece9b186e85047c74",
+    "url": "/deluxemenus-editor/index.html"
+  },
+  {
+    "revision": "1e79324faf32307eca02",
+    "url": "/deluxemenus-editor/static/css/5.f55c1f17.chunk.css"
+  },
+  {
+    "revision": "df52d3c93016b1d5755e",
+    "url": "/deluxemenus-editor/static/css/main.aed4dcdd.chunk.css"
+  },
+  {
+    "revision": "3f3b23eca1f1d984ecf5",
+    "url": "/deluxemenus-editor/static/js/0.3f1cb06a.chunk.js"
+  },
+  {
+    "revision": "24a564590cf5c51698ef",
+    "url": "/deluxemenus-editor/static/js/1.eb4de036.chunk.js"
+  },
+  {
+    "revision": "ba72f26c973094a1a3b3",
+    "url": "/deluxemenus-editor/static/js/2.209b3243.chunk.js"
+  },
+  {
+    "revision": "1e79324faf32307eca02",
+    "url": "/deluxemenus-editor/static/js/5.bc4f9e54.chunk.js"
+  },
+  {
+    "revision": "caff157271aa272844ee008c49f3f845",
+    "url": "/deluxemenus-editor/static/js/5.bc4f9e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df52d3c93016b1d5755e",
+    "url": "/deluxemenus-editor/static/js/main.01966c98.chunk.js"
+  },
+  {
+    "revision": "066a351cb6c9c39bb2b1",
+    "url": "/deluxemenus-editor/static/js/runtime-main.2afa244c.js"
+  }
+]);
